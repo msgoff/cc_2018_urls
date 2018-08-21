f430c6964c07acc8fa738519366d287be6c85ed0
Michael Goff <msgoff@users.noreply.github.com>
2018-08-21 19:12:20 +0000

    Add files via upload

--- download_gz_files.py
+++ download_gz_files.py
@@ -1,5 +1,9 @@
 import re
+<<<<<<< HEAD
+import urllib
+=======
 import requests
+>>>>>>> 4407173048c48268e6f7c2733f3c3a54bd3371e4
 import os
 
 f = open('2018_urls.dat')
@@ -16,7 +20,10 @@ for folder in folders:
         os.makedirs(folder)
 
 
+<<<<<<< HEAD
+=======
 import urllib
+>>>>>>> 4407173048c48268e6f7c2733f3c3a54bd3371e4
 
 def download_gz(url):
     f_name = re.split('/',url)[-1]
@@ -26,5 +33,10 @@ def download_gz(url):
              fullfilename = os.path.join(folder,f_name)
     urllib.urlretrieve(url, fullfilename)
 
+<<<<<<< HEAD
+for url in urls: 
+    download_gz(urls)
+=======
 download_gz(urls[0])
+>>>>>>> 4407173048c48268e6f7c2733f3c3a54bd3371e4
 
